35ef51af307a41d665a18a5a17291b5175cc42cd
monicahatis <monicaanestor@gmail.com>
2024-03-28 09:24:19 +0300

    Added call back function to producer

--- producer-app.py
+++ producer-app.py
@@ -6,9 +6,30 @@ producer_conf = {
 }
 producer = Producer(producer_conf)
 
-producer.produce('HelloWorld', key='1', value='Unique message from key 1')
+#Callback function to receive notification of delivery success or failure
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+
+
+msg = {
+
+    'Type': 'Truck 1',
+    'Log': '67,8N',
+    'Lat': '56.7s'
+}
+
+producer.produce(topic='Delivery', partition=0, value= str(msg), callback=acked)
 producer.poll(1)
+#Message cant be sent twice in the scenario it was sent the first time and didnt go(connection not established) but remained in queue. When the producer produces again the messages end up being two
+#producer.flush()
 
 
-# kafka-topics --bootstrap-server broker:29092 --create --topic HelloWorld 
+#in a real world we dont do this manually, creating a kafka topic:   kafka-topics --bootstrap-server broker:29092 --create --topic HelloWorld 
 # --if-not-exists --replication-factor 1 --partition 1
+
+
+##This file is not within the docker env
+#the producer will connect with the advertised listener
